f2b253e1086b044ecf679fc3d841fee2eadbb0e2
michalhagos <mhayele3@gmail.com>
2026-05-16 13:50:05 -0500

    Add a variable pointing to contracts.csv in resources folder

--- src/main/java/com/pluralsight/ContractFileManager.java
+++ src/main/java/com/pluralsight/ContractFileManager.java
@@ -2,6 +2,7 @@ package com.pluralsight;
 
 public class ContractFileManager {
 
-
+    // this is a csv file that holds all contracts.
+    private final String FILE_PATH = "src/main/resources/contracts.csv";
 
 }
