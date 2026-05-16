0192eb230c2e725ba25f3e5a388900b8880f090e
michalhagos <mhayele3@gmail.com>
2026-05-16 11:02:58 -0500

    Added constructor to LeaseContract that calculates expectedEndingValue and leaseFee

--- src/main/java/com/pluralsight/LeaseContract.java
+++ src/main/java/com/pluralsight/LeaseContract.java
@@ -7,6 +7,19 @@ public class LeaseContract extends Contract {
     // lease fee is 7% of the original vehicle price
     private double leaseFee;
 
+    // constructor calls the Contract parent constructor using super
+// then calculates and sets all the lease specific fields automatically
+    public LeaseContract(String date, String customerName, String customerEmail,
+                         Vehicle vehicleSold) {
+        // call the parent Contract constructor for all shared fields
+        super(date, customerName, customerEmail, vehicleSold);
+
+        // expected ending value is always 50% of the original price
+        this.expectedEndingValue = vehicleSold.getPrice() * 0.50;
+
+        // lease fee is always 7% of the original price
+        this.leaseFee = vehicleSold.getPrice() * 0.07;
+    }
 
 
 
