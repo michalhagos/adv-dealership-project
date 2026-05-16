e7e08e8264a8151289fb291a167eaa99763dc1bf
michalhagos <mhayele3@gmail.com>
2026-05-16 10:41:14 -0500

    created private fields expectedEndingValue and leaseFee to LeaseContract

--- src/main/java/com/pluralsight/LeaseContract.java
+++ src/main/java/com/pluralsight/LeaseContract.java
@@ -2,6 +2,15 @@ package com.pluralsight;
 
 public class LeaseContract extends Contract {
 
+    // expected ending value is 50% of the original vehicle price
+    private double expectedEndingValue;
+    // lease fee is 7% of the original vehicle price
+    private double leaseFee;
+
+
+
+
+
 
 
 }
